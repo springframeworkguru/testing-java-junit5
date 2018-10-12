d0e40f8a6c215288f93b75e193b15b6740cb16b5
John Thompson <john@springframework.guru>
2018-10-11 20:14:52 -0400

    adding dependent assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did not Match"),
+                        () -> assertEquals("Buck", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("1231231234", owner.getTelephone())
+                ));
+    }
+}
