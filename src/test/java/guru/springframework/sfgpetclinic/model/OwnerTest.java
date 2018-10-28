1d00e35454c3055b94478ec66a532389823fe5f1
John Thompson <john@springframework.guru>
2018-10-28 13:44:37 -0400

    adding csv source example

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -45,5 +46,16 @@ class OwnerTest implements ModelTests {
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "FL, 1, 1",
+            "OH, 2, 2",
+            "MI, 3, 1"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
