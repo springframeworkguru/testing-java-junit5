a2314065d5576e3db4308296b91a33397c0edde2
John Thompson <john@springframework.guru>
2018-10-28 13:56:54 -0400

    adding csvFile Example

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -56,6 +57,13 @@ class OwnerTest implements ModelTests {
             "MI, 3, 1"
     })
     void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
