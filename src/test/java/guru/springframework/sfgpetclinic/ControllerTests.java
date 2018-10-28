6f6386c9a87bc75fb10c9689287d639bab472ed5
John Thompson <john@springframework.guru>
2018-10-28 11:18:09 -0400

    adding default method example

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * Created by jt on 2018-10-28.
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Lets do something here");
+    }
 }
