cd6c6b29b3c2ac16b58a609fb6c39efb6fce7117
fbebek <finassistapp@gmail.com>
2020-11-05 05:36:54 +0100

    AssertJ with Junit

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -26,6 +27,8 @@ class IndexControllerTest {
         assertEquals("index", indexController.index());
         assertEquals("index", indexController.index(), "Wrong view returned");
         assertEquals("index", indexController.index(), () -> "Make me if you need to");
+
+        assertThat(indexController.index()).isEqualTo("index");
     }
 
     @Test
