c5b533aa3f9b3c6228c122903eacf3eb35138a13
hal90 <norrisj0612@gmail.com>
2021-07-12 05:24:16 -0400

    Added some assertEquals and an assertTrue test to indexController. Compiler warning of excessive lambda usage.

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    Author: jalnor
+    Date: 7/12/2021 5:08 AM
+    Project: guru.springframework.sfgpetclinic.controllers
+*/
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", indexController.index());
+        assertEquals("index", indexController.index(), "Wrong view returned");
+
+        assertEquals("index", indexController.index(), () -> "Another expensive message, " +
+                "make only if necessary!!!!!");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is some expensive " +
+                "message to build " +
+                "for my test");
+    }
+}
