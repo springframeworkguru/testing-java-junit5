69158d1fb6eea369b59493491a51c438b914a430
Alex <alex.rodrigues23997@gmail.com>
2022-05-20 13:17:27 -0300

    JUnit5 disabled tests feature

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp(){
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Test
+    @Disabled("Disabled intil we learn how to mock objects")
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
