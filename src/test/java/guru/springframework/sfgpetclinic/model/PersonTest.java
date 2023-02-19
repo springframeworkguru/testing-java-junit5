7298e6e0970ebbbb100b89d2230aa0d6ac3dbe84
Jesus Tapia <chucholuco@gmail.com>
2023-02-19 11:44:30 -0600

    adding repetition

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -30,5 +32,12 @@ class PersonTest implements ModelTests {
                 () -> assertEquals("Joe", person.getFirstName(),  "First Name Failed"),
                 () -> assertEquals("Buck", person.getLastName(),  "Last Name Failed")
         );
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test.")
+    @Test
+    void myRepeatedTest() {
+        // todo - impl
     }
 }
