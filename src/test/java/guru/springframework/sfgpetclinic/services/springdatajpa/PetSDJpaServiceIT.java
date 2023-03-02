00222ace159c685815a3b5524b0d7195f1a0da20
Jesus Tapia <chucholuco@gmail.com>
2023-03-01 23:46:13 -0600

    adding message

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ *
+ */
 @ExtendWith(TimingExtension.class)
 class PetSDJpaServiceIT {
 
