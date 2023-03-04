3d2bfcc4b5b681be345aaa064519cc1c6dabbc3d
mujtabaHossaini <mujtaba.hossaini94@gmail.com>
2023-03-04 10:02:16 +0330

    MODIFY: check order of execution

--- src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -24,14 +24,17 @@ class OwnerMapServiceTest {
         petTypeService = new PetTypeMapService();
         petService = new PetMapService();
         ownerMapService = new OwnerMapService(petTypeService, petService);
+
+        System.out.println("OwnerMapServiceTest- BeforeEach");
     }
 
     @DisplayName("Verify Zero Owners")
     @Test
     void ownersAreZero() {
         int ownerCount = ownerMapService.findAll().size();
-
         assertThat(ownerCount).isZero();
+
+        System.out.println("2-ownersAreZero");
     }
 
     @DisplayName("Pet Type - ")
@@ -44,14 +47,16 @@ class OwnerMapServiceTest {
             PetType petType2 = new PetType(2L, "Cat");
             petTypeService.save(petType);
             petTypeService.save(petType2);
+
+            System.out.println("Nested BeforeEach");
         }
 
         @DisplayName("Test Pet Count")
         @Test
         void testPetCount() {
             int petTypeCount = petTypeService.findAll().size();
-
             assertThat(petTypeCount).isNotZero().isEqualTo(2);
+            System.out.println("3-testPetCount");
         }
 
         @DisplayName("Save Owners Tests - ")
@@ -61,16 +66,18 @@ class OwnerMapServiceTest {
             @BeforeEach
             void setUp() {
                 ownerMapService.save(new Owner(1L, "Before", "Each"));
+
+                System.out.println("Nested Nested BeforeEach");
             }
 
             @DisplayName("Save Owner")
             @Test
             void saveOwner() {
                 Owner owner = new Owner(2L, "Joe", "Buck");
-
                 Owner savedOwner = ownerMapService.save(owner);
-
                 assertThat(savedOwner).isNotNull();
+
+                System.out.println("4-saveOwner");
             }
 
             @DisplayName("Save Owners Tests - ")
@@ -80,10 +87,10 @@ class OwnerMapServiceTest {
                 @DisplayName("Find Owner")
                 @Test
                 void findOwner() {
-
                     Owner foundOwner = ownerMapService.findById(1L);
-
                     assertThat(foundOwner).isNotNull();
+
+                    System.out.println("5-findOwner");
                 }
 
                 @DisplayName("Find Owner Not Found")
@@ -91,10 +98,19 @@ class OwnerMapServiceTest {
                 void findOwnerNotFound() {
 
                     Owner foundOwner = ownerMapService.findById(2L);
-
                     assertThat(foundOwner).isNull();
+
+                    System.out.println("6-findOwnerNotFound");
                 }
             }
+        }
+
+        @DisplayName("Test Pet Count")
+        @Test
+        void testOrderPetCount() {
+            int petTypeCount = petTypeService.findAll().size();
+            assertThat(petTypeCount).isNotZero().isEqualTo(2);
+            System.out.println("######################");
         }
     }
 
@@ -104,5 +120,17 @@ class OwnerMapServiceTest {
         int ownerCount = ownerMapService.findAll().size();
 
         assertThat(ownerCount).isZero();
+
+        System.out.println("1-ownersAreStillZero");
+    }
+
+    @DisplayName("Verify Order Still Zero Owners")
+    @Test
+    void ownersAreAreStillZero() {
+        int ownerCount = ownerMapService.findAll().size();
+
+        assertThat(ownerCount).isZero();
+
+        System.out.println("=================");
     }
 }
